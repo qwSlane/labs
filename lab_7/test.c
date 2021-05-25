aed88665259000a937f5be8b959aa6fa22830f02
vssqw <sergej.vasilewsckij@yandex.ru>
2021-05-25 12:40:41 +0300

    m

--- lab_7/test.c
+++ lab_7/test.c
@@ -7,28 +7,24 @@
 
 void templates()
 {
-    TourTemplate *templ;
+    
     List *cr;
     char *name, *route;
     float cost;
     int places, day, year, month;
+    readfile(&cr);
     places = day = year = month = 0;
     cost = 1;
     name = "test";
     route = "t->e->s->t";
-    templ = create(name, cost, places, route, day, year, month);
-    assert(templ != NULL);
-    add(&cr, templ);
     show(cr);
     assert(cr != NULL);
-    free(templ);
     free(cr);
     return;
 }
 
 int main()
 {
-
     templates();
     printf("Test succesfully completed");
     return 0;

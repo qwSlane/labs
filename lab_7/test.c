1a232a06aa26eac2289cfb6e7885ad38a2902fc7
vssqw <sergej.vasilewsckij@yandex.ru>
2021-05-25 12:44:26 +0300

    M

--- lab_7/test.c
+++ lab_7/test.c
@@ -7,17 +7,8 @@
 
 void templates()
 {
-    
     List *cr;
-    char *name, *route;
-    float cost;
-    int places, day, year, month;
     readfile(&cr);
-    places = day = year = month = 0;
-    cost = 1;
-    name = "test";
-    route = "t->e->s->t";
-    show(cr);
     assert(cr != NULL);
     free(cr);
     return;
